6382f759e4654400c5b5101e73a39ded7e76d615
Olly Pethick <ollypethick@gmail.com>
2022-03-27 14:06:30 +0100

    Add react countdown

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./App.css";
+import { Box, Typography } from "@mui/material";
+import Countdown from "react-countdown";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Box>
+        <Countdown
+          renderer={(formatted) => (
+            <Typography
+              suppressHydrationWarning
+              sx={{
+                fontSize: "1.5em",
+                fontFamily: "'Libre Baskerville', serif",
+                margin: "1.5rem 0",
+              }}
+            >{`${formatted.days} ${formatted.days === 1 ? "day" : "days"}, ${
+              formatted.hours
+            } ${formatted.hours === 1 ? "hour" : "hours"}, ${
+              formatted.minutes
+            } ${formatted.minutes === 1 ? "minute" : "minutes"}, ${
+              formatted.seconds
+            } ${formatted.seconds === 1 ? "second" : "seconds"}`}</Typography>
+          )}
+          date={Date.now() + 10000}
+        />
+      </Box>
     </div>
   );
 }
